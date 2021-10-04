2b42b1d63c2dfed477b9c67b41607ae5757d99b6
Matilde Ametrine <matilde@diffyq.xyz>
2021-10-04 11:03:21 -0400

    state.nix: remove `/glade`

--- state.nix
+++ state.nix
@@ -10,7 +10,6 @@ in {
   fileSystems = makeBindMounts {
     "/data" = "/nix/state/data";
     "/etc/nixos" = "/nix/state/nixos";
-    "/glade" = "/nix/state/glade";
     "/tmp" = "/nix/.tmp";
     "/var/lib/bluetooth" = "/nix/state/bluetooth";
     "/var/lib/yggdrasil" = "/nix/state/yggdrasil";
